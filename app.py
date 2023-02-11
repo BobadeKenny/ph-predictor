1866306c092a2b443ca284bdd3b152d3d760b038
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-02-11 05:45:29 +0100

    os env

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import requests
 import pickle
 import numpy as np
-import uuid
+import os
 
 app = Flask(__name__)
 anf = pickle.load(open('model/anfis.pkl', 'rb'))
@@ -27,5 +27,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.secret_key = uuid.uuid4().hex
+    app.secret_key = os.getenv('secret_key')
     app.run(debug=False, port=5000)
