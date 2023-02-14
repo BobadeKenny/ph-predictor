35999de28ce510060dbc783a06d41a247dcdd5ae
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-02-14 06:38:25 +0100

    changes

--- app.py
+++ app.py
@@ -18,15 +18,31 @@ def predict():
     longitude = request.form.get('longitude')
     url = "https://api.isda-africa.com/v1/soilproperty?key=AIzaSyCruMPt43aekqITCooCNWGombhbcor3cf4&lat={}&lon={}&property={}&depth=0-20".format(latitude, longitude, 'carbon_total')
     carbon_t = requests.get(url).json()
+    if carbon_t.get("message") is not None:
+        flash(carbon_t["message"])
+        return redirect(url_for('index'))
     ct = carbon_t['property']['carbon_total'][0]['value']["value"]
     url = "https://api.isda-africa.com/v1/soilproperty?key=AIzaSyCruMPt43aekqITCooCNWGombhbcor3cf4&lat={}&lon={}&property={}&depth=0-20".format(latitude, longitude, 'carbon_organic')
     carbon_o = requests.get(url).json()
     co = carbon_o['property']['carbon_organic'][0]['value']["value"]
     pred = anf.predict(np.array([[ct, co]]))
-    flash('The predicted soil pH value is %2.2f'% pred[0][0])
+    if pred < 4.4:
+        status = "extremely acidic"
+    elif pred < 5.5:
+        status = "strongly acidic"
+    elif pred < 6.5:
+        status = "slightly acidic"
+    elif pred < 7.3:
+        status = "neutral"
+    elif pred < 8.4:
+        status = "slightly alkaline"
+    else:
+        status = "strongly alkaline"
+    
+    flash('The soil pH is %s with a predicted value of %2.2f'% (status, pred[0][0]))
     return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     app.secret_key = os.environ.get('secret_key')
-    app.run(debug=False, port=5000)
+    app.run(debug=True, port=5000)
